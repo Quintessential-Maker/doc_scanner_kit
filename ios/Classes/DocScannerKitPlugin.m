4e30c983c4293720f375ea4ec7f709df2acc8ac1
Pallavii <developer.pallii@gmail.com>
2025-02-03 15:13:28 +0530

    ..

--- ios/Classes/DocScannerKitPlugin.m
+++ ios/Classes/DocScannerKitPlugin.m
@@ -1,6 +1,6 @@
 #import "DocumentScannerPlugin.h"
-#if __has_include(<doc_scanner_kit/doc_scanner_kit-Swift.h>)
-#import <doc_scanner_kit/doc_scanner_kit-Swift.h>
+#if __has_include(<doc_scanner_kit/DocScannerKitPlugin.h>)
+#import <doc_scanner_kit/DocScannerKitPlugin.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
