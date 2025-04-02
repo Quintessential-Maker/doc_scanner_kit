eafe48af3dcf53a810c02c2ac69613dfdd9d1aaa
Pallavii <developer.pallii@gmail.com>
2025-04-02 16:31:22 +0530

    ..

--- android/src/main/kotlin/com/quintessential/doc_scanner_kit/DocumentScannerPlugin.kt
+++ android/src/main/kotlin/com/quintessential/doc_scanner_kit/DocumentScannerPlugin.kt
@@ -21,7 +21,6 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class DocumentScannerPlugin : MethodCallHandler, ActivityResultListener,
     FlutterPlugin, ActivityAware {
